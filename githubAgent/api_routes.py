9ddd7527cef86f52cdefb522037cc6630ea6d194
osansinghrai <osanbantawarai@gmail.com>
2025-12-08 15:57:30 +0545

    feat: made the route to the agent summarizing the issue

--- githubAgent/api_routes.py
+++ githubAgent/api_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from .good_first_issue import router as GOOD_FIRST_ISSUE
 from .get_repo_activity import router as GET_REPO_ACTIVITY
+from .summarize_issue import router as SUMMARIZE_ISSUE
 
 router = APIRouter()
 
@@ -9,3 +10,6 @@ router.include_router(GOOD_FIRST_ISSUE, prefix="/good-first-issue")
 
 # GET REPO ACTIVITY
 router.include_router(GET_REPO_ACTIVITY, prefix="/get-repo-activity")
+
+# SUMMARIZE ISSUE
+router.include_router(SUMMARIZE_ISSUE, prefix="/summarize-issue")
